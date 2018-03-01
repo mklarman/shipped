4c3cbf41f48dd07be3029dd3a4085525e937097e
SheilaWaples <sheilawaples78@gmail.com>
2018-03-01 17:06:21 -0500

    started on boat controller and views

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,29 @@
 class BoatsController < ApplicationController
+	def index
+		@boat = Boat.all
+	end
+
+	def new
+		@boat =Boat.new
+	end
+
+	def create
+		boat = Boat.new(boat_params)
+	end
+
+	def show
+	end
+	
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
+
+ private
+
+# def
