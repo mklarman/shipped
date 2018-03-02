23332bb5371bb34f2beae23a05c36ffb296662cc
DorisCCLin <doriscclin@gmail.com>
2018-03-02 13:45:33 -0500

    update limitation added

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,34 +2,41 @@ class JobsController < ApplicationController
 
 
 
-def create
-	job = Job.new(job_params)
-	if(job.cost < 100)
-		flash[:message] = 'All Jobs Must Cost Min $100'
+# def create
+# 	job = Job.new(job_params)
+# 	if(job.cost < 100)
+# 	flash[:message] = 'All Jobs Must Cost Min $100'
+# end
 
 
+
+def show
+        @job = Job.find_by_id(params[:id])
 end
 
-
-
-	def show
-        @job = Job.find_by_id(params[:id])
-	end
-
-	def update
-		job = Job.find_by_id(params[:id])
-		if job.update(job_params)
-        	redirect_to "/jobs/#{job.id}"
+def update
+	job = Job.find_by_id(params[:id])
+	if(job_params[:cost].to_i < 100)
+		flash[:message] = 'All Jobs Must Cost Min $100'
+		redirect_to "/jobs/#{job.id}"
+	elsif (job_params[:description].length < 20)
+		flash[:message] = 'You description need to have at least 20 charactors'
+		redirect_to "/jobs/#{job.id}"	 	
+	else	
+		if job.update(job_params)	
+        flash[:message] = 'updated successfully'
+        redirect_to "/jobs/#{job.id}"
         else
         	render "/jobs/#{job.id}"
         end
-	end	
+    end    
+end	
 
-	def destroy
-		job = Job.find_by_id(params[:id])
-        job.destroy
-        redirect_to '/'
-	end
+def destroy
+    job = Job.find_by_id(params[:id])
+    job.destroy
+    redirect_to '/'
+end
 
 private
     def job_params
