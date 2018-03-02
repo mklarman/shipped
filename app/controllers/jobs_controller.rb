663a6611ed3518ed97c7e7580058aa5a340f8267
DorisCCLin <doriscclin@gmail.com>
2018-03-02 11:28:21 -0500

    Job update/delete added

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,24 @@
 class JobsController < ApplicationController
+	def show
+        @job = Job.find_by_id(params[:id])
+	end
+
+	def update
+		job = Job.find_by_id(params[:id])
+		if job.update(job_params)
+        	redirect_to "/jobs/#{job.id}"
+        else
+        	render "/jobs/#{job.id}"
+        end
+	end	
+
+	def destroy
+		job = Job.find_by_id(params[:id])
+        job.destroy
+        redirect_to '/'
+	end
+private
+    def job_params
+    	params.require(:job).permit(:description, :origin, :destination, :cost, :containers)
+    end	
 end
