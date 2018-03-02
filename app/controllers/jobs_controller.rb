de74f3d0ffc19606f62ab82892c38c8fdfc32fb1
mklarman <mklarman@hotmail.com>
2018-03-02 11:46:27 -0500

    pushing to test conflicts

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,19 @@
 class JobsController < ApplicationController
+
+
+def create
+
+
+end
+
+
+
+private
+    def job_params
+        params.require(:job).permit(:description, :origin, :destination, :cost, :containers)
+    end
+
+
+
+
 end
