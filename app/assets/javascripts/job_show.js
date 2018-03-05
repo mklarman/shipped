427403648f59a455ba41595051fb3dafda719962
DorisCCLin <doriscclin@gmail.com>
2018-03-05 16:00:11 -0500

    jobShow styled

---
+++ app/assets/javascripts/job_show.js
@@ -0,0 +1,15 @@
+console.log('hello');
+
+window.onload = function(){
+var gear = document.getElementById('edit_gear');
+var edit = document.getElementById('job_edit_form');
+
+gear.addEventListener('click', function(){
+	if (edit.style.display==''){
+	edit.style.display='block'
+    } else {
+		edit.style.display=''
+	}
+})
+
+}
