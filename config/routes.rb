61b15319d7782e6c8f5703956434c1f4136b8229
ildarmgt <ildarmgt@gmail.com>
2018-03-05 12:52:33 -0500

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
- devise_for :users
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
- post '/assign', to: "boats#assign"
- resources :users
- resources :boats
- resources :jobs
+  post '/assign', to: "boats#assign"
+  resources :users
+  resources :boats
+  resources :jobs
+
+  authenticated do
+    root :to => 'users#show'
+  end
  
   devise_scope :user do
- 	get '/', to: 'devise/sessions#new' 
- 	root  "devise/sessions#new"
- end
+    get '/', to: 'devise/sessions#new' 
+    root  "devise/sessions#new"
+  end
 end
 
 
