4964de176be11ffede5cb89b73f275af50d0769f
ildarmgt <ildarmgt@gmail.com>
2018-03-06 07:18:34 -0500

    styling user page complete

--- app/assets/javascripts/users_show.js
+++ app/assets/javascripts/users_show.js
@@ -1,3 +1,76 @@
 window.onload = () => {
-  console.log('finally');
+  // users#show scripts go here
+  var FORM_NEW_BOAT = document.getElementsByClassName('main__users__boats__form')[0];
+  var BTN_SHOW_FORM_NEW_BOAT = document.getElementsByClassName('main__users__boats__btn_add')[0];
+  var BOATS_ITEM = document.getElementsByClassName('main__users__boats__item');
+  var BTN_DELETE = document.getElementsByClassName('main__users__boats__item_delete');
+  var CONFIRM_BTNS = document.getElementsByClassName('main__users__boats__item_delete_end');
+  var CANCEL_BTNS = document.getElementsByClassName('main__users__boats__item_cancel');
+  
+
+  if (!! BTN_SHOW_FORM_NEW_BOAT) {
+    BTN_SHOW_FORM_NEW_BOAT.onclick = () => {
+      FORM_NEW_BOAT.style.display = (FORM_NEW_BOAT.style.display == 'inline-block')
+        ? FORM_NEW_BOAT.style.display = 'none' 
+        : FORM_NEW_BOAT.style.display = 'inline-block';
+      
+    }
+  };
+  
+  // for each boat list item
+  [].forEach.call(BOATS_ITEM, (ea_item) => {
+    // add a mouse movement event
+    ea_item.onmouseenter = (this_event) => {
+      // grab boat id # from attribute field
+      let boatid = this_event.currentTarget.getAttribute('data-boatid');
+      
+      // grab the div with boat options in focus
+      let BOATS_DELETE_START_THIS = document.querySelectorAll('.main__users__boats__item_delete_start[data-boatid="' + boatid + '"]')[0];
+
+      // grab all the other divs with boat options focus or not
+      let BOATS_DELETE_START_ALL = document.querySelectorAll('.main__users__boats__item_delete_start');
+      
+      // remove option visibility everywhere
+      [].forEach.call(BOATS_DELETE_START_ALL, (clean_each_item) => {
+        clean_each_item.style.display = 'none';
+      });
+
+      [].forEach.call(CONFIRM_BTNS, (clean_each_item) => {
+        clean_each_item.style.display = 'none';
+      });
+
+      // and make the mouse overed options the only ones visible
+      BOATS_DELETE_START_THIS.style.display = 'block';
+    }
+  });
+
+  // for each delete button display confirm or cancel
+  [].forEach.call(BTN_DELETE, (ea_btn_delete) => {
+    ea_btn_delete.onclick = (this_event) => {
+      // grab boat id # from event
+      let boatid = this_event.currentTarget.getAttribute('data-boatid');
+
+      // hide current button
+      this_event.currentTarget.parentElement.style.display = 'none';
+      
+      // make the confirm buttons visible
+      let BOATS_DELETE_SHOW_CONFIRM = document.querySelectorAll('.main__users__boats__item_delete_end[data-boatid="' + boatid + '"]')[0];
+      BOATS_DELETE_SHOW_CONFIRM.style.display = 'block';
+    }
+  });
+
+  // for each cancel button, hide confirms 
+  [].forEach.call(CANCEL_BTNS, (ea_btn_cancel) => {
+    ea_btn_cancel.onclick = (this_event) => {
+      // grab boat id # from event
+      let boatid = this_event.currentTarget.getAttribute('data-boatid');
+
+      // hide current button
+      this_event.currentTarget.parentElement.style.display = 'none';
+
+    }
+  });
+
 }
+
+
--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -9,52 +9,54 @@ class BoatsController < ApplicationController
 
 	def create
 		boat = Boat.new(boat_params)
-		if boat.save
-			redirect_to "/boats"
-		else
-			redirect_to new_boat_path
+    if boat.save
+      flash[:message] = 'New ship added'
+			redirect_back(fallback_location: root_path)
+    else
+      flash[:message] = 'New ship not added'
+			redirect_back(fallback_location: root_path)
 		end	
 	end
 
 	def show
-		@boat = Boat.find(params[:id])
+		@boat = Boat.find_by_id(params[:id])
 		@jobs = Job.all
-	  @boat = Boat.find_by_id(params[:id])
+	  # @boat = Boat.find_by_id(params[:id])
 	end
 	
 	def edit
-		@boat = Boat.find(params[:id])
+		@boat = Boat.find_by_id(params[:id])
 		
 	end
 
 	def update
-		@boat = Boat.find(params[:id])
+		@boat = Boat.find_by_id(params[:id])
 		@boat = Boat.update
 	end
 
 	def destroy
-		@boat = Boat.find(params[:id])
-		@boat = Boat.destroy
-		redirect_to '/boats'
+		this_boat = Boat.find_by_id(params[:id])
+		this_boat.destroy
+		redirect_to root_path
 	end
 
 
-    def assign
-    	@job = Job.find(params[:id])
-		@boat = Boat.find(params[:boat_id])
-		if @job.boats.find_by_id(@boat.id)
-            flash[:message] = 'this boat has been assigned'
-            redirect_to "/jobs/#{@job.id}"
-		else
-			@job.boats << @boat
-			redirect_to "/jobs/#{@job.id}"
-		end	
-    end	
+  def assign
+    @job = Job.find(params[:id])
+  @boat = Boat.find(params[:boat_id])
+  if @job.boats.find_by_id(@boat.id)
+          flash[:message] = 'this boat has been assigned'
+          redirect_to "/jobs/#{@job.id}"
+  else
+    @job.boats << @boat
+    redirect_to "/jobs/#{@job.id}"
+  end	
+  end	
 
  private
 
- def boat_params
- 	params.require(:boat).permit(:name, :capacity, :location, :avatar, :user_id)
- end
+  def boat_params
+    params.require(:boat).permit(:name, :capacity, :location, :avatar, :user_id)
+  end
 end
 
